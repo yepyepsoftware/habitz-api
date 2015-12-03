d8941454219e1413e8a8324021024b1a16314f5d
Pauli Siirama <pauli@Paulis-MacBook-Pro.local>
2015-12-03 07:32:07 +0200

    Moved client to another repo.

--- client/js/pages/habitz/habitz.js
+++
@@ -1,6 +0,0 @@
-angular.module('habitz', ['ngRoute'])
-  .config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/habitz/', {
-      template: '<habitz></habitz>'
-    });
-  }]);
--- client/js/pages/users/users.js
+++
@@ -1,6 +0,0 @@
-angular.module('users', ['ngRoute'])
-  .config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/users/', {
-      template: '<users></users>'
-    });
-  }]);
